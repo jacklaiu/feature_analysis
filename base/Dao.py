4636df8b0d4cb7da8b9d311937845084b934d3bc
lb <jacklaiu@qq.com>
2018-07-19 22:42:49 +0800

    update

--- base/Dao.py
+++ base/Dao.py
@@ -1,13 +1,36 @@
+import pymysql.cursors
 
+# Connect to the database
+connection = pymysql.connect(host='95.163.200.245',
+                             user='feature_analysis',
+                             password='queue11235813',
+                             db='feature_analysis',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
+def update(sql, values):
+    cursor = None
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+            cursor.execute(sql, values)
+        # connection is not autocommit by default. So you must commit to save
+        # your changes.
+        connection.commit()
+    finally:
+        cursor.close()
 
+def select(sql, values):
+    cursor = None
+    try:
+        with connection.cursor() as cursor:
+            # Read a single record
+            cursor.execute(sql, values)
+            result = cursor.fetchall()
+            return result
 
-
-
-
-
-
-
+    finally:
+        cursor.close()
 
 
 
---
+++ base/DateUtil.py
@@ -0,0 +1,31 @@
+import time
+import datetime
+
+def getYMDHMS():
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+def getYMD():
+    return time.strftime("%Y-%m-%d", time.localtime())
+
+def getHMS():
+    return time.strftime("%H:%M:%S", time.localtime())
+
+def getTimeStamp():
+    millis = int(round(time.time() * 1000))
+    return millis
+
+def getFormatToday():
+    return time.strftime("%Y-%m-%d", time.localtime())
+
+def getPreDayYMD(num=1, startdate=None):
+    today=datetime.date.today()
+    if startdate is not None:
+        arr = startdate.split("-")
+        today = datetime.date(int(arr[0]), int(arr[1]), int(arr[2]))
+    oneday=datetime.timedelta(days=num)
+    d=today-oneday
+    return str(d)
+
+import moment
+
+print(moment.now().add(1, 'days').format('YYYY-M-D'))
