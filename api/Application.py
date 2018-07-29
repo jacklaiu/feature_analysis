7168ef1f86e71f3e9941518e34d24dede87986e1
lb <jacklaiu@qq.com>
2018-07-30 01:16:51 +0800

    update

---
+++ api/Application.py
@@ -0,0 +1,21 @@
+# -*- coding: gbk -*-
+from flask import Flask, abort, request, jsonify
+import analysis.concept.Common as ay_com
+app = Flask(__name__, static_folder='/web', static_url_path='/web')
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/get_zhangting_concepts/<dayCount>')
+def get_zhangting_concepts(dayCount):
+    map = ay_com.get_zhangtingconcept_countMap(dayCount)
+    return jsonify(map)
+
+@app.route('/view/get_zhangting_concepts/')
+def view_get_zhangting_concepts():
+    return app.send_static_file('get_zhangting_concepts.html')#homepage.html在static文件夹下
+
+if __name__ == '__main__':
+    app.run()
